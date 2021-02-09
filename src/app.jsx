c72cfff25f01f29f30bceba9ed4a672ff54af9da
wangkobong <wangkobong@gamil.com>
2021-02-10 00:21:41 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,27 @@
 
 import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
-
-import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos))  
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAp5NhuYNfvOx016mMogvyNZMCm8xNArds", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos))
+    .catch(error => console.log('error', error));
   }, []);
 
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search}/>
       <VideoList videos={videos} />
     </div>
   );
